4df66ebaea8d4bb803d1338309227992b57e2e30
Kei Homma <homma22000@cs.saisoncard.co.jp>
2024-06-21 16:25:15 +0900

    不要importの削除

--- inventory/src/main/java/com/example/inventory/InventoryApplication.java
+++ inventory/src/main/java/com/example/inventory/InventoryApplication.java
@@ -1,9 +1,7 @@
 package com.example.inventory;
 
-import com.example.inventory.mapper.ItemMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class InventoryApplication {
