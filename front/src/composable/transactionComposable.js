e596ada0a55e5fd86f88926d8445596c941c77c0
Kei Homma <homma22000@cs.saisoncard.co.jp>
2024-06-26 15:36:08 +0900

    1件登録メソッドの追加

---
+++ front/src/composable/transactionComposable.js
@@ -0,0 +1,35 @@
+import { ref } from 'vue';
+import { inventoryComposable} from "@/composable/inventoryComposable.js";
+const { fetchInventories } = inventoryComposable();
+
+export function transactionComposable() {
+    const baseUrl = 'http://localhost:8080';
+    const transactions = ref([]);
+    const transaction = ref(null);
+    const error = ref(null);
+
+    // 在庫取引の1件登録
+    const createTransaction = async (newTransaction) => {
+        try {
+            const response = await fetch(`${baseUrl}/api/transactions`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(newTransaction)
+            });
+            if (response.status === 404) throw new Error('指定した部品コードのリソースが存在しない');
+            if (response.status === 409) throw new Error('在庫数量不足による登録失敗');
+            if (!response.ok) throw new Error(`Error: ${response.status}`);
+        } catch (err) {
+            error.value = err.message;
+            throw err;
+        } finally {
+            await fetchInventories();
+        }
+    };
+
+    return {
+        createTransaction
+    }
+}
