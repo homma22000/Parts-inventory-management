87a5fdf1267e27bf51ddcef1c65a2a8299b48028
Kei Homma <homma22000@cs.saisoncard.co.jp>
2024-06-28 03:31:33 +0900

    在庫数量が不足の場合は登録が失敗する条件を追加

--- inventory/src/main/java/com/example/inventory/service/TransactionService.java
+++ inventory/src/main/java/com/example/inventory/service/TransactionService.java
@@ -5,4 +5,6 @@ import com.example.inventory.entity.Transaction;
 public interface TransactionService {
 
     void register(Transaction transaction);
+
+    int getCurrentQuantity(String itemCode);
 }
