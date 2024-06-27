b2643a6eb1a72ee840445ea70776ca63084c446c
Kei Homma <homma22000@cs.saisoncard.co.jp>
2024-06-27 09:17:47 +0900

    作業中断

---
+++ inventory/src/main/java/com/example/inventory/mapper/TransactionMapper.java
@@ -0,0 +1,12 @@
+package com.example.inventory.mapper;
+
+import com.example.inventory.entity.Transaction;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface TransactionMapper {
+    @Insert("insert into inventory_transactions(type, date_time, item_code, quantity, description)" +
+            " values(#{type}, #{dateTime}, #{item.code}, #{quantity}, #{description}")
+    int insert(Transaction transaction);
+}
